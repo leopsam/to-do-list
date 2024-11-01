30a0b4b1c479dfa6d14681b444e010d77b662ff6
Leonardo Pereira Sampaio <leonardo.lps@outlook.com.br>
2024-11-01 01:45:59 -0300

    refactor: App.jsx, create Loading component and start Home page

---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+import { MainContainer } from './styles'
+
+export function Home() {
+    return (
+        <MainContainer>
+           <Text>Bem vindo a home</Text>
+        </MainContainer>
+    )
+}
